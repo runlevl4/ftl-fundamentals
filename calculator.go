f7d813e5a3742a7267eebcf9e155520a737e7eb8
runlevl4 <root@runlevl4.com>
2020-09-26 17:57:26 -0400

    Added Divide func.

--- calculator.go
+++ calculator.go
@@ -1,6 +1,11 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"errors"
+	"math"
+)
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -16,4 +21,14 @@ func Subtract(a, b float64) float64 {
 // together.
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+// Divide takes two numbers and returns the result of dividing them.
+func Divide(a, b float64) (float64, error) {
+	x := a / b
+
+	if x == math.NaN() {
+		return 0, errors.New("invalid input")
+	}
+	return x, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -6,9 +6,10 @@ import (
 )
 
 type testCase struct {
-	a, b float64
-	want float64
-	name string
+	a, b        float64
+	want        float64
+	name        string
+	errExpected bool
 }
 
 func TestAdd(t *testing.T) {
@@ -20,7 +21,7 @@ func TestAdd(t *testing.T) {
 		{a: 2, b: 8, want: 10, name: "Adding larger number to smaller."},
 		{a: -4, b: 8, want: 4, name: "Adding negative and positive numbers."},
 	}
-	
+
 	for _, tc := range testCases {
 		t.Log(tc.name)
 		got := calculator.Add(tc.a, tc.b)
@@ -47,8 +48,8 @@ func TestSubtract(t *testing.T) {
 		if tc.want != got {
 			t.Errorf("want %f, got %f", tc.want, got)
 		}
-	}	
-	
+	}
+
 }
 
 func TestMultiply(t *testing.T) {
@@ -63,9 +64,35 @@ func TestMultiply(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Log(tc.name)
-		got := calculator.Multiply(tc.a, tc.b)	
+		got := calculator.Multiply(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("want %f, got %f", tc.want, got)
 		}
-	}	
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{a: 24, b: 3, want: 8, name: "Standard division."},
+		{a: 3, b: 2, want: 1.5, name: "Fractional division."},
+		{a: 24, b: 0, want: 0, name: "Divide by zero.", errExpected: true},
+	}
+
+	for _, tc := range testCases {
+		t.Log(tc.name)
+		switch tc.errExpected {
+		case true:
+			_, err := calculator.Divide(tc.a, tc.b)
+			if err != nil {
+				t.Errorf("want %v, got %s", nil, err)
+			}
+		default:
+			got, _ := calculator.Divide(tc.a, tc.b)
+			if tc.want != got {
+				t.Errorf("want %f, got %f", tc.want, got)
+			}
+		}
+	}
 }
